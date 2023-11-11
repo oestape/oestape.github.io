e02b677919b1a2c309208036eb2f8910e5cac759
Octavi Estapé <oestape@users.noreply.github.com>
2023-11-11 07:01:45 +0100

    Update doku.php

--- octavi/doku.php
+++ octavi/doku.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <meta http-equiv="refresh" content="0; url='https://www.activovision.com'" />
+    <meta http-equiv="refresh" content="1; url='https://www.activovision.com'" />
   </head>
   <body>
     <p>You will be redirected to www.activovision.com</p>
