78762faf97d27bd5037395ad32e89567b1574719
Octavi Estapé <oestape@users.noreply.github.com>
2023-11-12 17:21:13 +0100

    Delete octavi/doku.php

--- octavi/doku.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta http-equiv="refresh" content="1; url='https://www.activovision.com'" />
-  </head>
-  <body>
-    <p>You will be redirected to www.activovision.com</p>
-  </body>
-</html>
